2e72ec9ff0d3975a099a3a26ff58f4cde35fd465
ElMarlon <elmarlon8a@gmail.com>
2024-05-30 18:11:20 -0500

    primer cambio

---
+++ app/Http/Controllers/ClienteController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cliente;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ClienteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $clientes=Cliente::all();
+        return view('clientes.index', compact('clientes'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('clientes.create', compact('cliente'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $cliente = Cliente::create($request->all());
+        return redirect()->route('clientes.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        $cliente = Cliente::findOrFail($id);
+        $ventas = $cliente->ventas()->get();
+        return view('clientes.show', compact('ventas'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit($id)
+    {
+        $cliente = Cliente::findOrFail($id);
+        return view('clientes.edit', compact('cliente'));
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Cliente $cliente, $id)
+    {
+        $cliente = Cliente::findOrFail($id);
+        $cliente->update($request->all());
+        return redirect()->route('clientes.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        $cliente = Cliente::findOrFail($id);
+        $cliente->delete();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\ClienteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
 });
+
+Route::get('/clientes',[ClienteController::class, 'index']);
+Route::post('/clientes',[ClienteController::class,'store'])->name('clientes.store');
+Route::get('/clientes/create',[ClienteController::class,'create'])->name('clientes.create');
+Route::get('/clientes/{id}',[ClienteController::class,'show'])->name('clientes.show');
